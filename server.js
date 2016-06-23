5f41d31834bf4ca0e15645153aecabffb0f2f78d
Jay Arnel Bilocura <jay.arnel@gmail.com>
2016-06-23 17:54:12 +0800

    added initial server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('static'));
+
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
